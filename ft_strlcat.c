28d2e6960cccd2ef94f6d2918c1135187d370c8e
Marwane Ahansal <ahansalmarwane@gmail.com>
2022-10-09 05:52:56 +0100

    fixed some issues at ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -14,23 +14,44 @@
 
 size_t  ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-    size_t  dst_len;
-    size_t  index;
-    size_t  len;
+    // size_t  dst_len;
+    // size_t  index;
+    // size_t  len;
 
-    dst_len = ft_strlen(dst);
-    index = dst_len;
-    len = 0;
-    if (dstsize > 0 && dstsize >= dstsize - dst_len - 1)
-    {
-        while (src[len] != '\0' && len <= dstsize - dst_len - 1)
-        {
-            dst[index] = src[len];
-            len++;
-            index++;
-        }
-        dst[len] = '\0';
-    }
-    return (dst_len + ft_strlen(src));
-    //return (ft_strlen(dst));
+    // dst_len = ft_strlen(dst);
+    // index = dst_len;
+    // len = 0;
+    // if (dstsize > 0 && dstsize >= dstsize - dst_len - 1)
+    // {
+    //     while (src[len] != '\0' && len <= dstsize - dst_len - 1)
+    //     {
+    //         dst[index] = src[len];
+    //         len++;
+    //         index++;
+    //     }
+    //     dst[len] = '\0';
+    // }
+    // return (dst_len + ft_strlen(src));
+
+	size_t	i;
+	size_t	j;
+	size_t	dest_size;
+	size_t	src_size;
+
+	i = 0;
+	dest_size = ft_strlen(dst);
+	src_size = ft_strlen(src);
+	j = dest_size;
+	if (dstsize > dest_size)
+	{
+		while (src[i] != '\0' && i < dstsize - dest_size - 1)
+		{
+			dst[j] = src[i];
+			i++;
+			j++;
+		}
+		dst[j] = '\0';
+		return (dest_size + src_size);
+	}
+	return (src_size + dstsize);
 }
--- main.c
+++ main.c
@@ -7,8 +7,8 @@ int main(void)
 	char dst1[4] = "";
 	char dst2[4] = "";
 	char src[] = "thx to ntoniolo for this test !";
-	printf("strlcat res: %zu\n", strlcat(dst1, src, 4));
-	printf("ft_strlcat res: %zu\n", ft_strlcat(dst2, src, 4));
+	printf("strlcat res: %zu\n", strlcat(NULL, src, 0));
+	printf("ft_strlcat res: %zu\n", ft_strlcat(NULL, src, 0));
 	printf("dst1: %s\n", dst1);
 	printf("dst2: %s\n", dst2);
 	return (0);
