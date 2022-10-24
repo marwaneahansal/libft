4bf71e107d57220b5552ff15d153d040563082bb
Marwane Ahansal <ahansalmarwane@gmail.com>
2022-10-24 07:04:16 +0100

    fixed norminette in ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,29 +6,42 @@
 /*   By: mahansal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 07:57:59 by mahansal          #+#    #+#             */
-/*   Updated: 2022/10/24 04:40:40 by mahansal         ###   ########.fr       */
+/*   Updated: 2022/10/24 07:03:45 by mahansal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
+size_t	get_allocation_len(char const *s, unsigned int start, size_t len)
+{
+	size_t	tmp_len;
+	int		index;
+	size_t	s_len;
+
+	tmp_len = 0;
+	index = start;
+	s_len = ft_strlen(s);
+	while (start < s_len && s[index] != '\0' && tmp_len < len)
+	{
+		index++;
+		tmp_len++;
+	}
+	return (tmp_len);
+}
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	unsigned int	index;
 	char			*substring;
 	size_t			s_len;
-	unsigned long long			tmp_len;
 
 	index = 0;
 	s_len = 0;
-	tmp_len = 0;
 	if (s == 0)
 		return (0);
 	s_len = ft_strlen(s);
-	while (s[tmp_len] != '\0' && tmp_len < len)
-		tmp_len++;
-	substring = malloc((tmp_len + 1) * sizeof(char));
+	substring = malloc((get_allocation_len(s, start, len) + 1) * sizeof(char));
 	if (substring == 0)
 		return (0);
 	if (start < s_len)
--- main.c
+++ main.c
@@ -5,32 +5,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-void *toupperStr(void *s)
-{
-	char *s1 = (char *) s;
-	int index = 0;
-	char *str = malloc((ft_strlen(s1) + 1) * sizeof(char));
-	if (!str)
-		return (0);
-	while(s1[index] != '\0')
-	{
-		if (s1[index] >= 97 && s1[index] <= 122)
-		{
-			str[index] = s1[index] - 32;
-		}
-		index++;
-	}
-	str[index] = '\0';
-	return (str);
-}
-
-void deleteNodeContent(void *str)
-{
-	free(str);
-}
-
 int main(void)
 {
-	printf("%s\n", ft_strtrim("test", 0));
+	printf("%s\n", ft_substr("a", 1, 1));
 	return (0);
 }
